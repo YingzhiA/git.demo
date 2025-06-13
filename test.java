07712513aec3ccf1db734acea6b7fa302413031a
yingzhia <hongrui.gao@geneseeq.com>
2025-06-13 09:52:42 +0800

    测试提交

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println(10/0);
+        int a = 10;
+        int b = 0;
+        System.out.println(a/b);
     }
 }
