26dc9e3a31d8c02e6f49f769796df2d0328969da
yingzhia <hongrui.gao@geneseeq.com>
2025-06-13 13:30:12 +0800

    测试

--- test4.java
+++ test4.java
@@ -1,10 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
-        System.out.println(a/b);
-        int c;
-        int d;
         System.out.println(c/d);
     }
 }
